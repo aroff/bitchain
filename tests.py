f7729e3675d0005360c6360de5f11811a62005bb
Alexandre <alexandre@55seeds.com>
2020-01-21 00:35:17 -0300

    code coverage setup

--- tests.py
+++ tests.py
@@ -1,9 +1,10 @@
 from FieldElement import FieldElement
 from Point import Point
 
-a = FieldElement(1, 12)
-b = FieldElement(2, 12)
-c = FieldElement(3, 12)
-c == a + b
+a = FieldElement(0, 223)
+b = FieldElement(7, 223)
+x = FieldElement(15, 223)
+y = FieldElement(86, 223)
 
-x = Point(1, 1, 2, 2)
+p = Point(x, y, a, b)
+#print(7 * p)

b219d58b6e4c52ba21f0af6b16f12ae0c31eedf3
Alexandre <alexandre@55seeds.com>
2020-01-21 00:32:43 -0300

    code coverage

--- tests.py
+++ tests.py
@@ -1,7 +1,9 @@
 from FieldElement import FieldElement
-from FieldElement import Point
+from Point import Point
 
 a = FieldElement(1, 12)
 b = FieldElement(2, 12)
 c = FieldElement(3, 12)
 c == a + b
+
+x = Point(1, 1, 2, 2)
